77f6c8fbae8e9c7def028a9041fede33e08f2e09
Rafael <rafawillkomm@gmail.com>
2023-10-02 11:19:46 -0300

    troca de "breeza" para "goku PM"

--- .vscode/script.js
+++ .vscode/script.js
@@ -4,14 +4,19 @@ function toggleMode() {
 
   // pegar a imagem 
   const img = document.querySelector('#profile img')
+  const p = document.querySelector('#profile p')
 
   // substituir a imagem
-  if(html.classList.contains('light')){
+  if(html.classList.contains('light')) {
     img.setAttribute('src', './assets/avatar-light.jpg')
+    // muda o nome para Goku PM
+    p.textContent = "Goku PM"
   } else {
     // se estiver sem o light mode 
     img.setAttribute('src', '././assets/profile.jpg')
+    p.textContent = 'Breeza'
   }  
+
 
 }
 
